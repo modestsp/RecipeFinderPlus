ca3b254d1dca1af2c41910a1868ce6c19d52f3b3
srsebbi <csperichon@gmail.com>
2023-05-30 21:42:37 -0300

    Added PostgreSQL connection

--- RecipeFinderPlusAPI/Program.cs
+++ RecipeFinderPlusAPI/Program.cs
@@ -1,12 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using RecipeFinderPlusAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
