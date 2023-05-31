71bb6ea2055dfa1db9d9f905f999c79336210016
srsebbi <csperichon@gmail.com>
2023-05-30 21:41:52 -0300

    Created Recipe Model

---
+++ RecipeFinderPlusAPI/Models/Recipe.cs
@@ -0,0 +1,19 @@
+namespace RecipeFinderPlusAPI.Models;
+
+public class Recipe
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Likes { get; set; }
+    //     public string? Description { get; set; }
+    //     public string? Instructions { get; set; }
+    //     public string? Image { get; set; }
+    //     public string? Category { get; set; }
+    //     public int? Time { get; set; }
+    //     public int? Calories { get; set; }
+    //     public int? Servings { get; set; }
+    //     public int? UserId { get; set; }
+    // }
+}
+
+
