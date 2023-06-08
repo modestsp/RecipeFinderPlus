d575c1a993b369d8a4269dd3717772e7a88f8ce3
srsebbi <csperichon@gmail.com>
2023-06-08 20:40:24 -0300

    second migration

---
+++ RecipeFinderPlusAPI/Migrations/20230514235630_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RecipeFinderPlusAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
---
+++ RecipeFinderPlusAPI/Migrations/20230608212131_secondMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeFinderPlusAPI.Migrations
+{
+    public partial class secondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Recipes",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Likes",
+                table: "Recipes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Likes",
+                table: "Recipes");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Recipes",
+                newName: "Name");
+        }
+    }
+}
