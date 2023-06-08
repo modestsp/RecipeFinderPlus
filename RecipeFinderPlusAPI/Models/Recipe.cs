fd59728a0ffe53b904a55e9392fd52ece3a921a3
srsebbi <csperichon@gmail.com>
2023-06-08 20:42:28 -0300

    Changed Name for Title on Recipe Model

--- RecipeFinderPlusAPI/Models/Recipe.cs
+++ RecipeFinderPlusAPI/Models/Recipe.cs
@@ -3,7 +3,7 @@ namespace RecipeFinderPlusAPI.Models;
 public class Recipe
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string? Title { get; set; }
     public int Likes { get; set; }
     //     public string? Description { get; set; }
     //     public string? Instructions { get; set; }
