cb472b6010c862a7ab73cc302ec739414ff5c30f
srsebbi <csperichon@gmail.com>
2023-06-08 20:41:44 -0300

    Added healthchekcs and recipeservice scope

--- RecipeFinderPlusAPI/Program.cs
+++ RecipeFinderPlusAPI/Program.cs
@@ -1,20 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using RecipeFinderPlusAPI.Data;
+using RecipeFinderPlusAPI.Services.Recipe;
 
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddScoped<IRecipeService, RecipeService>();
+
+    var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+    builder.Services.AddHealthChecks();
+}
+
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseNpgsql(connectionString));
 var app = builder.Build();
-
+app.MapHealthChecks("/health");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
