448ade7aaa234306274d40d24c2964109dc56e24
srsebbi <csperichon@gmail.com>
2023-06-09 19:21:18 -0300

    Added Get single recipe service

--- RecipeFinderPlusAPI/Services/Recipe/IRecipeService.cs
+++ RecipeFinderPlusAPI/Services/Recipe/IRecipeService.cs
@@ -3,6 +3,7 @@ namespace RecipeFinderPlusAPI.Services.Recipe;
 
 public interface IRecipeService
 {
+    Task<Models.Recipe> GetRecipeByIdAsync(int id);
     Task<ICollection<Models.Recipe>> GetTrendingRecipesAsync();
     void AddRecipe(Models.Recipe recipe);
     Task<Models.Recipe> UpdateRecipeAsync(Models.Recipe recipe);
--- RecipeFinderPlusAPI/Services/Recipe/RecipeService.cs
+++ RecipeFinderPlusAPI/Services/Recipe/RecipeService.cs
@@ -14,6 +14,11 @@ public class RecipeService : IRecipeService
             throw new ArgumentNullException(nameof(context));
     }
 
+    public async Task<Models.Recipe> GetRecipeByIdAsync(int id)
+    {
+        return await _context.Recipes.FirstOrDefaultAsync(r => r.Id == id);
+    }
+
     public async Task<ICollection<Models.Recipe>> GetTrendingRecipesAsync()
     {
         return await _context.Recipes.OrderByDescending(r => r.Likes).Take(10).ToListAsync();
@@ -29,9 +34,11 @@ public class RecipeService : IRecipeService
         _context.Recipes.Add(recipeToAdd);
     }
 
-    public Task<Models.Recipe> UpdateRecipeAsync(Models.Recipe recipe)
+    public async Task<Models.Recipe> UpdateRecipeAsync(Models.Recipe recipe)
     {
-        throw new NotImplementedException();
+        var existingRecipe = await _context.Recipes.FirstOrDefaultAsync(r => r.Id == recipe.Id);
+        existingRecipe.Likes++;
+        return existingRecipe;
     }
 
     public async Task<bool> SaveChangesAsync()
